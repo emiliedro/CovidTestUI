73ee951d7ed0693ed19cd8a21e98921ec8ebed25
emiliedro <64741768+emiliedro@users.noreply.github.com>
2020-07-22 16:27:11 -0400

    I have added the pages for the patient's usser interface. I have also added a navigation bar to the bottom of each page.

--- app/src/main/java/com/example/covidtestui/PatientLogin.java
+++ app/src/main/java/com/example/covidtestui/PatientLogin.java
@@ -2,13 +2,32 @@ package com.example.covidtestui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientLogin extends AppCompatActivity {
+    Button buttonPatientSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
+
+        buttonPatientSignIn = (Button) findViewById(R.id.buttonPatientSignIn);
+
+        buttonPatientSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientHome();
+            }
+        });
+    }
+
+    public void openPatientHome(){
+        Intent intent = new Intent(this, PatientHome.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/covidtestui/PatientMore.java
@@ -0,0 +1,88 @@
+package com.example.covidtestui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PatientMore extends AppCompatActivity {
+    Button buttonHome, buttonBookTest, buttonTestResults, buttonSymptoms, buttonMore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_more);
+
+        buttonHome = (Button) findViewById(R.id.buttonHome);
+        buttonBookTest = (Button) findViewById(R.id.buttonBookTest);
+        buttonTestResults = (Button) findViewById(R.id.buttonTestResults);
+        buttonSymptoms = (Button) findViewById(R.id.buttonSymptoms);
+        buttonMore = (Button) findViewById(R.id.buttonMore);
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientHome();
+            }
+        });
+
+        buttonBookTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBookTest();
+            }
+        });
+
+        buttonTestResults.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openTestResults();
+            }
+        });
+
+        buttonSymptoms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSymptomLog();
+            }
+        });
+
+        buttonMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientMore();
+            }
+        });
+    }
+
+    public void openPatientHome(){
+        Intent intent = new Intent(this, PatientHome.class);
+        startActivity(intent);
+
+    }
+
+    public void openBookTest(){
+        Intent intent = new Intent(this, BookTest.class);
+        startActivity(intent);
+
+    }
+    public void openTestResults(){
+        Intent intent = new Intent(this, TestResults.class);
+        startActivity(intent);
+
+    }
+
+    public void openSymptomLog(){
+        Intent intent = new Intent(this, SymptomLog.class);
+        startActivity(intent);
+
+    }
+
+    public void openPatientMore(){
+        Intent intent = new Intent(this, PatientMore.class);
+        startActivity(intent);
+
+    }
+}
