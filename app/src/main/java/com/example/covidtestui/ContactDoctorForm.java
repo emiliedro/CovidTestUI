8b0d1b81013d21ac695075e3b8b1f2e1a63498a6
emiliedro <64741768+emiliedro@users.noreply.github.com>
2020-07-23 01:46:58 -0400

    Finished adding all pages to the Patient's user interface

--- app/src/main/java/com/example/covidtestui/ContactDoctorForm.java
+++ app/src/main/java/com/example/covidtestui/ContactDoctorForm.java
@@ -2,13 +2,41 @@ package com.example.covidtestui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactDoctorForm extends AppCompatActivity {
+
+    Button buttonContactDoctorSubmit, buttonContactDoctorCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_doctor_form);
+
+        buttonContactDoctorCancel = (Button) findViewById(R.id.buttonContactTracingCancel);
+        buttonContactDoctorSubmit = (Button) findViewById(R.id.buttonContactTracingSubmit);
+
+        buttonContactDoctorSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientMore();
+            }
+        });
+
+        buttonContactDoctorCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientMore();
+            }
+        });
+    }
+
+    public void openPatientMore(){
+        Intent intent = new Intent(this, PatientMore.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/covidtestui/ContactTracingForm.java
+++ app/src/main/java/com/example/covidtestui/ContactTracingForm.java
@@ -2,13 +2,40 @@ package com.example.covidtestui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactTracingForm extends AppCompatActivity {
+    Button buttonContactTracingCancel, buttonContactTracingSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_tracing_form);
+
+        buttonContactTracingCancel = (Button) findViewById(R.id.buttonContactTracingCancel);
+        buttonContactTracingSubmit = (Button) findViewById(R.id.buttonContactTracingSubmit);
+
+        buttonContactTracingSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientMore();
+            }
+        });
+
+        buttonContactTracingCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPatientMore();
+            }
+        });
+    }
+
+    public void openPatientMore(){
+        Intent intent = new Intent(this, PatientMore.class);
+        startActivity(intent);
+
     }
 }
